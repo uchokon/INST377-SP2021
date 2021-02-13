428b53ed786b6e4fe38266aa377c20684f866034
uchokon <69696089+uchokon@users.noreply.github.com>
2021-02-12 22:06:50 -0500

    Lab 3 CSS fixed and array added

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,38 +1,24 @@
+/* eslint-disable prefer-const */
+/* eslint-disable indent */
+/* eslint-disable max-len */
+/* eslint-disable no-multiple-empty-lines */
 /* eslint-disable func-names */
 /* eslint-disable no-plusplus */
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable no-console */
 /* Put your javascript in here */
 
-// const li = document.querySelectorAll('li');
+const carouselArray = [];
 
-// for (const li of document.querySelectorAll('li')) {
-//   li.style.position = 'relative';
-//   li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
-//   i++;
-// }
+const imgs = document.querySelectorAll('.gallery li');
+const arrow = document.querySelectorAll('.arrow');
 
-// /* configuration */
-// const width = 130; // image width
-// const count = 3; // visible images count
+for (let i = 0; i < imgs.length; i++) {
+    carouselArray.push(imgs[i]);
+}
 
-// const list = document.querySelector('ul');
-// const listElems = document.querySelectorAll('li');
+arrow.onclick = function() {
+    console.log('CLCIKED');
+};
 
-// let position = 0; // ribbon scroll position
-
-// document.querySelector('.prev').onclick = function() {
-//   // shift left
-//   position += width * count;
-//   // can't move to the left too much, end of images
-//   position = Math.min(position, 0);
-//   list.style.marginLeft = `${position}px`;
-// };
-
-// document.querySelector('.next').onclick = function() {
-//   // shift right
-//   position -= width * count;
-//   // can only shift the ribbbon for (total ribbon length - visible count) images
-//   position = Math.max(position, -width * (listElems.length - count));
-//   list.style.marginLeft = `${position}px`;
-// };
+console.log(carouselArray[5]);
