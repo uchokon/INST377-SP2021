7455c9ebcdc3da4e4c37ef18f8e88a06fa9b3d2a
uchokon <69696089+uchokon@users.noreply.github.com>
2021-02-12 22:52:01 -0500

    Lab 3 Completed

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,3 +1,7 @@
+/* eslint-disable prefer-template */
+/* eslint-disable space-before-blocks */
+/* eslint-disable quotes */
+/* eslint-disable no-alert */
 /* eslint-disable prefer-const */
 /* eslint-disable indent */
 /* eslint-disable max-len */
@@ -8,17 +12,37 @@
 /* eslint-disable no-console */
 /* Put your javascript in here */
 
-const carouselArray = [];
+let width = 130;
+let count = 3;
+let position = 0; // ribbon scroll position
 
+const list = document.querySelector('ul');
+const listElems = document.querySelectorAll('li');
 const imgs = document.querySelectorAll('.gallery li');
-const arrow = document.querySelectorAll('.arrow');
+const prev = document.querySelectorAll('.prev');
+const next = document.querySelectorAll('.next');
+
+const carouselArray = [];
 
 for (let i = 0; i < imgs.length; i++) {
     carouselArray.push(imgs[i]);
 }
 
-arrow.onclick = function() {
-    console.log('CLCIKED');
-};
+function movePrev() {
+    // shift left
+    position += width * count;
+    // can't move to the left too much, end of images
+    position = Math.min(position, 0);
+    list.style.marginLeft = position + 'px';
+}
 
-console.log(carouselArray[5]);
+function moveNext() {
+    // shift right
+    position -= width * count;
+    // can only shift the ribbbon for (total ribbon length - visible count) images
+    position = Math.max(position, -width * (listElems.length - count));
+    list.style.marginLeft = position + 'px';
+}
+
+prev[0].addEventListener("click", movePrev);
+next[0].addEventListener("click", moveNext);
